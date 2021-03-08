cd9576b80b7fdb65c850adddd67d80e943944184
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-08 16:20:42 +0100

    POST

--- js challenge/graph.js
+++ js challenge/graph.js
@@ -1,11 +1,14 @@
-    setInterval(function timer() {
-        let xhr = new XMLHttpRequest;
-        xhr.open('GET', 'https://canvasjs.com/services/data/datapoints.php', true)
-        xhr.onload = function() {
-            if (this.status === 200) {
-                let result = (JSON.parse(this.responseText));
-                console.log(result);
-            }
+let xhr = new XMLHttpRequest;
+
+setInterval(() => {
+    xhr.open('POST', 'https://canvasjs.com/services/data/datapoints.php', true)
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let result = this.responseText;
+            console.log(result);
+        } else if (this.status === 404) {
+            console.log('ERROR 404 RETRY');
         }
-        xhr.send();
-    }, 1000);
+    }
+    xhr.send();
+}, 1000);
