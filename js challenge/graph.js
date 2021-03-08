bd313563d9abc52f5ab448022d95d574fe52ef5d
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-08 16:36:34 +0100

    change result

--- js challenge/graph.js
+++ js challenge/graph.js
@@ -1,10 +1,10 @@
 let xhr = new XMLHttpRequest;
-
+let result;
 setInterval(() => {
     xhr.open('POST', 'https://canvasjs.com/services/data/datapoints.php', true)
     xhr.onload = function () {
         if (this.status === 200) {
-            let result = this.responseText;
+            result = this.responseText;
             console.log(result);
         } else if (this.status === 404) {
             console.log('ERROR 404 RETRY');
