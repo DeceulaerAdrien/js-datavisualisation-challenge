8aae2205815b76deda08620639856d45484b8b89
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-09 11:09:38 +0100

    canvas installer

--- js challenge/graph.js
+++ js challenge/graph.js
@@ -1,13 +1,30 @@
 let xhr = new XMLHttpRequest;
 let result;
-let canvas = document.createElement('canvas')
-let divC = document.querySelector('#bodyContent')
+let canvas1 = document.createElement('canvas')
+let canvas2 = document.createElement('canvas')
+let canvas3 = document.createElement('canvas')
+let divA = document.querySelector('#bodyContent');
+let divB = document.querySelector('#mw-content-text');
 
-divC.insertBefore(canvas, divC.firstChild)
-document.getElementsByTagName('canvas')[0].setAttribute("id", "canvas1");
-document.getElementById('canvas1').setAttribute("width", "400");
-document.getElementById('canvas1').setAttribute("height", "400");
+const canvasA = () => {
+    divA.insertBefore(canvas1, divA.firstChild);
+    divA.firstChild.setAttribute("id", "canvas1");
+    document.getElementById('canvas1').setAttribute("width", "400");
+    document.getElementById('canvas1').setAttribute("height", "400");
+}
 
+const canvasB = () => {
+    divB.insertBefore(canvas2, divB.childNodes[10]);
+    divB.childNodes[10].setAttribute("id", "canvas2");
+    document.getElementById('canvas2').setAttribute("width", "400");
+    document.getElementById('canvas2').setAttribute("height", "400");
+}
+const canvasC = () => {
+    divB.insertBefore(canvas3, divB.childNodes[38]);
+    divB.childNodes[38].setAttribute("id", "canvas3");
+    document.getElementById('canvas3').setAttribute("width", "400");
+    document.getElementById('canvas3').setAttribute("height", "400");
+}
 setInterval(() => {
     xhr.open('POST', 'https://canvasjs.com/services/data/datapoints.php', true)
     xhr.onload = function () {
@@ -20,3 +37,7 @@ setInterval(() => {
     }
     xhr.send();
 }, 1000);
+
+canvasA();
+canvasB();
+canvasC();
